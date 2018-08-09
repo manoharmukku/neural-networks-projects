0e6a3311d9d5b269edee505cf580ebd328456cb3
Manohar Mukku <manoharmukku@gmail.com>
2018-08-09 11:39:23 +0530

    Added and commented code for 3D data

--- assign1.py
+++ assign1.py
@@ -117,18 +117,13 @@ plt.scatter(class2_input_data[:,0], class2_input_data[:,1], c='green', marker='o
 plt.plot(x, y, 'k-')
 plt.show()
 
-
-#### For 3D (3 features) data ####
-
-# # Parameters
+### For 3D data ###
+# Get the parameters from command line
 # m = 3 # Number of features
-# n = 10000 # Number of samples
-# learning_rate = 0.1
-# max_iterations = 10000 # Maximum number of iterations
 
 # # Randomly generate two dimensional data using Normal distribution for two linearly separable classes
-# mean1, sd1 = (8.0, 10.0, 9.0), 1.0 # Mean and standard deviation for class 1 3d features
-# mean2, sd2 = (-7.0, -6.0, -6.5), 1.0 # Mean and standard deviation for class 2 3d features
+# mean1, sd1 = (5.0, 6.0, 7.0), 1.0 # Mean and standard deviation for class 1 2d features
+# mean2, sd2 = (-1.0, -0.5, -0.6), 1.0 # Mean and standard deviation for class 2 2d features
 # class1_input_data = np.random.normal(mean1, sd1, ((int)(0.5*n), m)) # (5000, 3) matrix having desired output label +1
 # class2_input_data = np.random.normal(mean2, sd2, ((int)(0.5*n), m)) # (5000, 3) matrix having desired output label -1
 
@@ -140,12 +135,14 @@ plt.show()
 # # Contatenate all into a single numpy nd-array
 # class1_data = np.concatenate((bias_input_term, class1_input_data, class1_desired_labels), axis = 1)
 # class2_data = np.concatenate((bias_input_term, class2_input_data, class2_desired_labels), axis = 1)
+
 # data = np.concatenate((class1_data, class2_data), axis = 0)
 
 # # Randomly initialize free parameters between -0.05 and 0.05
-# weight_vector = np.random.uniform(low = -0.05, high = 0.05, size = (m+1, 1)) # (m+1, 1) weight vector including the bias term
+# weight_vector = np.random.uniform(low = -100.0, high = -99.0, size = (m+1, 1)) # (m+1, 1) weight vector including the bias term
+# weight_vector[0] = -1000.0
 
-# print ("============================")
+# print ("\n============================")
 # print ("3D data")
 # print ("============================\n")
 
@@ -154,36 +151,50 @@ plt.show()
 # print ("Number of samples in class 2: {}".format((int)(0.5*n)))
 # print ("Learning rate: {}".format(learning_rate))
 
-# print ("\nTraining on 3D data...\n")
+# print ("\nTraining on 3D data:")
+# print ("----------------------")
 
-# # print ("--------")
+# fig = plt.figure()
 
-# # for i in range(max_iterations):
-# # stdout.write ("\rIteration %d of %d (max)"%(i+1, max_iterations))
-# # stdout.flush()
+# for i in range(max_iterations):
+#     stdout.write ("\rIteration %d of %d (max)"%(i+1, max_iterations))
+#     stdout.flush()
 
-# # Save the weights of this iteration
-# # weights_prev = weight_vector
+#     # Save the weights of this iteration
+#     weights_prev = weight_vector.copy()
 
-# # Randomly shuffle the data
-# np.random.shuffle(data)
+#     # Plot the decision boundary before this iteration
+#     b = weight_vector[0][0]
+#     w1 = weight_vector[1][0]
+#     w2 = weight_vector[2][0]
+#     x = np.linspace(-3, 7)
+#     y = (-w1/w2) * x + (-b/w2)
 
-# # Iterate over all the rows of the data and train the perceptron model
-# for row in data:
-#     input_vector = row[0:m+1].T.reshape(m+1, 1) # (m+1, 1) input vector including +1 bias term
-#     desired_output = row[m+1] # Desired output corresponding to the input vector, +1 or -1
+#     plt.scatter(class1_input_data[:,0], class1_input_data[:,1], c='blue', marker = '+')
+#     plt.scatter(class2_input_data[:,0], class2_input_data[:,1], c='green', marker='o')
+#     plt.plot(x, y, 'k-')
+#     plt.pause(0.00001)
+#     plt.close()
 
-#     # Calculate actual output
-#     actual_output = signum((weight_vector.T).dot(input_vector))
+#     # Randomly shuffle the data
+#     np.random.shuffle(data)
 
-#     # Update the weights
-#     weight_vector += learning_rate * (desired_output - actual_output) * input_vector
+#     # Iterate over all the rows of the data and train the perceptron model
+#     for row in data:
+#         input_vector = row[0:m+1].T.reshape(m+1, 1) # (m+1, 1) input vector including +1 bias term
+#         desired_output = row[m+1] # Desired output corresponding to the input vector, +1 or -1
 
-# # If no change in weights from previous iteration
-# # if (np.array_equal(weight_vector.reshape(m+1), weights_prev.reshape(m+1))):
-# #     break
+#         # Calculate actual output
+#         actual_output = signum((weight_vector.T).dot(input_vector))
 
-# print ("Done training\n")
+#         # Update the weights
+#         weight_vector += (learning_rate * (desired_output - actual_output) * input_vector)
+
+#     # If no change in weights from previous iteration
+#     if (np.array_equal(weight_vector.flatten(), weights_prev.flatten())):
+#         break
+
+# print ("\nDone training\n")
 
 # # Print the equation of the decision bounday
 # print ("Equation of decision boundary:")
@@ -191,6 +202,13 @@ plt.show()
 # b = weight_vector[0][0]
 # w1 = weight_vector[1][0]
 # w2 = weight_vector[2][0]
-# w3 = weight_vector[3][0]
-# print ("({}) * x1 + ({}) * x2 + ({}) * x3 + ({}) = 0\n".format(w1, w2, w3, b))
+# print ("({}) * x1 + ({}) * x2 + ({}) = 0\n".format(w1, w2, b))
 
+# # Plot the final decision bounday
+# x = np.linspace(-3, 7)
+# y = (-w1/w2) * x + (-b/w2)
+
+# plt.scatter(class1_input_data[:,0], class1_input_data[:,1], c='blue', marker = '+')
+# plt.scatter(class2_input_data[:,0], class2_input_data[:,1], c='green', marker='o')
+# plt.plot(x, y, 'k-')
+# plt.show()
