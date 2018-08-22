2d115d0614b621018853a3ac00785a53ff213c74
Manohar Mukku <manoharmukku@gmail.com>
2018-08-22 21:26:08 +0530

    Create k_means file

---
+++ assign3/k_means.py
@@ -0,0 +1,12 @@
+# Author: Manohar Mukku
+# Date: 22.08.2018
+# Desc: K-means clustering implementation
+
+class k_means_clustering(object):
+    def __init__(self):
+        return None
+
+    def get_input(self, n_clusters, n_features, input_data):
+        self.K = n_clusters
+        self.n = n_features
+        
