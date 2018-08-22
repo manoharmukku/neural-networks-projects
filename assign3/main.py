397e0c1cea84fbf5b4654dac99f88594bc079e60
Manohar Mukku <manoharmukku@gmail.com>
2018-08-22 21:05:17 +0530

    Create main

---
+++ assign3/main.py
@@ -0,0 +1,14 @@
+# Author: Manohar Mukku
+# Date: 22.08.2018
+# Desc: Radial Basis Function Network (RBFN)
+
+from rbfn import *
+import sys
+from sys import stdout
+import numpy as np
+import pandas as pd
+
+if (len(sys.argv) != )
+    print ("Usage: python main.py ")
+    sys.exit()
+
