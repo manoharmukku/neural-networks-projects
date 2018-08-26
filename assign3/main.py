694e67d71b0ace359c2dea11e25823133f75f661
Manohar Mukku <manoharmukku@gmail.com>
2018-08-26 12:33:10 +0530

    Minor changes

--- assign3/main.py
+++ assign3/main.py
@@ -2,11 +2,13 @@
 # Date: 22.08.2018
 # Desc: Radial Basis Function Network (RBFN)
 
-from rbfn import *
 import sys
-from sys import stdout
 import numpy as np
 import pandas as pd
+import random
+
+from rbfn import *
+from k_means import *
 
 if (len(sys.argv) != )
     print ("Usage: python main.py ")
--- assign3/rbfn.py
+++ assign3/rbfn.py
@@ -2,3 +2,7 @@
 # Date: 22.08.2018
 # Desc: Radial Basis Function Network (RBFN)
 
+class RBFN(object):
+    def __init__(self, ):
+
+    
